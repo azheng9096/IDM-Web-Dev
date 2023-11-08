5ad8fdf2da0ba3f811267103a764adcb8848c1d9
Anna Zheng <26550446+azheng9096@users.noreply.github.com>
2023-11-08 11:33:59 -0500

    to do list

---
+++ week08/script.js
@@ -0,0 +1,48 @@
+const submit_btn = document.querySelector("#to-do-submit");
+const to_do_input = document.querySelector("#toDoInput");
+let to_do_list = document.querySelector("#to-do-list");
+
+submit_btn.addEventListener("click", makeList);
+
+function makeList() {
+  let to_do_input_val = to_do_input.value;
+  let to_do_container = document.createElement("div");
+  to_do_container.className = "to-do-item-container";
+
+  let to_do_item = document.createElement("label");
+  to_do_item.className = "to-do-item";
+  let to_do_checkbox = document.createElement("input");
+  to_do_checkbox.setAttribute("type", "checkbox");
+  to_do_checkbox.addEventListener("click", () => completeItem(to_do_checkbox));
+
+  let text_container = document.createElement("span");
+  text_container.appendChild(document.createTextNode(to_do_input_val));
+
+  let delete_button = document.createElement("button");
+  let delete_icon = document.createElement("span");
+  delete_icon.className = "material-symbols-outlined";
+  delete_icon.textContent = "delete";
+  delete_button.appendChild(delete_icon);
+  delete_button.addEventListener("click", () => deleteItem(delete_button));
+
+  to_do_list.appendChild(to_do_container);
+  to_do_container.appendChild(to_do_item);
+  to_do_item.appendChild(to_do_checkbox);
+  to_do_item.appendChild(text_container);
+  to_do_container.appendChild(delete_button);
+
+  to_do_input.value = "";
+}
+
+function deleteItem(e) {
+  e.parentNode.remove();
+}
+function completeItem(e) {
+  let text_container = e.parentNode.querySelector("span");
+
+  if (e.checked) {
+    text_container.classList.add("to-do-completed");
+  } else {
+    text_container.classList.remove("to-do-completed");
+  }
+}
